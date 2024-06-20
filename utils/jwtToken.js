b18d39294083a044fbfedc147b012168c6496850
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2024-06-20 03:46:12 +0100

    modification

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -18,6 +18,8 @@ export const generateToken = (user, message, statusCode, res) => {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
+    sameSite: "None",
+      secure: true,
   };
 
   res.status(statusCode).cookie('token', token, options).json({
