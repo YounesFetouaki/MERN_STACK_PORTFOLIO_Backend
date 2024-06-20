476eda5ece3e12d3f66e5ea0be4c8838517f7b1c
YounesFetouaki <younesfetouaki65@gmail.com>
2024-06-20 03:14:14 +0100

    fix

--- controller/userController.js
+++ controller/userController.js
@@ -193,7 +193,7 @@ export const updatePassword = catchAsyncErrors(async (req, res, next) => {
 });
 
 export const getUserForPortfolio = catchAsyncErrors(async (req, res, next) => {
-  const id = "663296a896e553748ab5b0be";
+  const id = "6673703fa9ddfe67666ac270";
   const user = await User.findById(id);
   res.status(200).json({
     success: true,
